9e97fb20cb03df8460c57aa4e48f4c86de55f17c
SebastianAlvaradoMa <106775864+SebastianAlvaradoMa@users.noreply.github.com>
2022-08-05 18:12:24 -0600

    Update Especie.java
    Especie terminado

--- Proyecto Zoo/src/proyecto/zoo/Especie.java
+++ Proyecto Zoo/src/proyecto/zoo/Especie.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package proyecto.zoo;
 
-/**
- *
- * @author sebas
- */
+import javax.swing.JOptionPane;
+
 public class Especie {
+    public class Animal {
+   public String especie;
+   public String estado;
+   public String carac;
+   
+    public void leerDatos(){
+        carac=JOptionPane.showInputDialog(null,
+                "Digite las características de la especie:");
+        especie=JOptionPane.showInputDialog(null,
+                "Digite la especie que ingresa:");
+        estado=JOptionPane.showInputDialog(null,
+                "1.Estado activo\n2.Estado Inactivo\nInserte el estado de la especie en el zoologico:");
+        
+        if("1".equals(estado)){
+            estado="Estado Activo";
+            
+        }else{
+            estado="Estado Inactivo";
+            }
+    }
+        
+    public void mostrarDatos(){
+        JOptionPane.showMessageDialog(null,
+                "El nombre de la especie es: "+especie+"\nLas características de la especie son: "+carac+"\nEstado de la especie: "+estado);
+        }
+              
+        
     
+    
+}
 }
